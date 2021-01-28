bfefc76edd98505b9b6dfa5f5f16cbda73b52e07
TheHelias <akande.tolu@yahoo.com>
2021-01-28 22:56:51 +0100

    feat: complete validate-rule endpoint

--- server/routes/index.js
+++ server/routes/index.js
@@ -17,7 +17,6 @@ router.get('/', (req, res, next) => {
 })
 
 router.post('/validate-rule', async (req, res, next) => {
-  let field
   let fieldValue
   const payload = req.body
   const ruleKeys = ['field', 'condition', 'condition_value']
@@ -28,120 +27,123 @@ router.post('/validate-rule', async (req, res, next) => {
       status: 'error',
       data: null
     })
-
-  if (!payload.rule) {
-    genericErrorResponse('rule is required.')
-    return
-  }
-
-  if (!payload.data) {
-    genericErrorResponse('data is required.')
-    return
-  }
-
-  if (typeof payload.rule !== 'object') {
-    genericErrorResponse('rule should be an object.')
-    return
-  }
-
-  if (
-    typeof payload.data !== 'object' ||
-    Array.isArray(payload.data) === false ||
-    typeof payload.data !== 'string' ||
-    payload.data === null
-  ) {
-    genericErrorResponse('rule should be an object, array or string.')
-    return
-  }
-
-  for (let i = 0; i < ruleKeys.length; i++) {
-    if (!payload.rule.hasOwnProperty(ruleKeys[i])) {
-      genericErrorResponse(`${ruleKeys[i]} is required.`)
-      break
-    }
-    return
-  }
-
-  if (payload.rule.field.includes('.')) {
-    const nestings = payload.rule.field.split('.')
-    if (nestings.length > 2) {
-      genericErrorResponse('Invalid field.')
-      return
-    } else if (!payload.data[nestings[0]][nestings[1]]) {
-      genericErrorResponse(`field ${nestings[0]} is missing from data.`)
+  try {
+    if (!payload.rule) {
+      genericErrorResponse('rule is required.')
       return
     }
-    field = await nestings[1]
-    fieldValue = await payload.data[nestings[0]][nestings[1]]
-  } else {
-    field = await payload.rule.field
-    fieldValue = await payload.data[payload.rule.field]
-  }
 
-  if (!validConditions.includes(payload.rule.condition)) {
-    genericErrorResponse("condition isn't valid.")
-  }
-
-  const ruleValidationResponse = (error, condition) => {
-    const data = {
-      validation: {
-        error: error,
-        field: field,
-        field_value: fieldValue,
-        condition: condition,
-        condition_value: payload.rule.condition_value
-      }
+    if (!payload.data) {
+      genericErrorResponse('data is required.')
+      return
     }
-    if (error) {
-      res.status(400).send({
-        message: `field ${field} failed validation.`,
-        status: 'error',
-        data: data
-      })
+
+    if (typeof payload.rule !== 'object') {
+      genericErrorResponse('rule should be an object.')
+      return
+    }
+
+    if (
+      (typeof payload.data === 'object' && payload.data !== null) ||
+      typeof payload.data === 'string'
+    ) {
+      true
     } else {
-      res.status(200).send({
-        message: `field ${field} successfully validated.`,
-        status: 'success',
-        data: data
-      })
+      genericErrorResponse('data should be an object, array or string.')
+      return
     }
-  }
+    console.log(0)
+    for (let i = 0; i < ruleKeys.length; i++) {
+      if (!payload.rule.hasOwnProperty(ruleKeys[i])) {
+        genericErrorResponse(`${ruleKeys[i]} is required.`)
+        return
+      }
+    }
+    console.log(1)
+    if (payload.rule.field.includes('.')) {
+      const nestings = payload.rule.field.split('.')
+      if (nestings.length > 2) {
+        genericErrorResponse('rule field is invalid.')
+        return
+      } else if (!payload.data[nestings[0]][nestings[1]]) {
+        genericErrorResponse(`field ${nestings[0]} is missing from data.`)
+        return
+      }
+      fieldValue = await payload.data[nestings[0]][nestings[1]]
+    } else if (!payload.data[payload.rule.field]) {
+      genericErrorResponse(`field ${payload.rule.field} is missing from data.`)
+      return
+    } else {
+      fieldValue = await payload.data[payload.rule.field]
+    }
+    console.log(2)
+    if (!validConditions.includes(payload.rule.condition)) {
+      genericErrorResponse("condition isn't valid.")
+    }
+    console.log(fieldValue)
+    const ruleValidationResponse = (error, condition) => {
+      const data = {
+        validation: {
+          error: error,
+          field: payload.rule.field,
+          field_value: fieldValue,
+          condition: condition,
+          condition_value: payload.rule.condition_value
+        }
+      }
+      if (error) {
+        res.status(400).send({
+          message: `field ${payload.rule.field} failed validation.`,
+          status: 'error',
+          data: data
+        })
+      } else {
+        res.status(200).send({
+          message: `field ${payload.rule.field} successfully validated.`,
+          status: 'success',
+          data: data
+        })
+      }
+    }
 
-  switch (payload.rule.condition) {
-    case 'eq':
-      if (fieldValue === payload.rule.condition_value) {
-        ruleValidationResponse(false, 'eq')
-      } else {
-        ruleValidationResponse(true, 'eq')
-      }
-      break
-    case 'neq':
-      if (fieldValue !== payload.rule.condition_value) {
-        ruleValidationResponse(false, 'neq')
-      } else {
-        ruleValidationResponse(true, 'neq')
-      }
-      break
-    case 'gt':
-      if (fieldValue > payload.rule.condition_value) {
-        ruleValidationResponse(false, 'gt')
-      } else {
-        ruleValidationResponse(true, 'gt')
-      }
-      break
-    case 'gte':
-      if (fieldValue >= payload.rule.condition_value) {
-        ruleValidationResponse(false, 'gte')
-      } else {
-        ruleValidationResponse(true, 'gte')
-      }
-      break
-    case 'contains':
-      if (fieldValue.includes(payload.rule.condition_value)) {
-        ruleValidationResponse(false, 'contains')
-      } else {
-        ruleValidationResponse(true, 'contains')
-      }
+    switch (payload.rule.condition) {
+      case 'eq':
+        if (fieldValue === payload.rule.condition_value) {
+          ruleValidationResponse(false, 'eq')
+        } else {
+          ruleValidationResponse(true, 'eq')
+        }
+        break
+      case 'neq':
+        if (fieldValue !== payload.rule.condition_value) {
+          ruleValidationResponse(false, 'neq')
+        } else {
+          ruleValidationResponse(true, 'neq')
+        }
+        break
+      case 'gt':
+        if (fieldValue > payload.rule.condition_value) {
+          ruleValidationResponse(false, 'gt')
+        } else {
+          ruleValidationResponse(true, 'gt')
+        }
+        break
+      case 'gte':
+        if (fieldValue >= payload.rule.condition_value) {
+          ruleValidationResponse(false, 'gte')
+        } else {
+          ruleValidationResponse(true, 'gte')
+        }
+        break
+      case 'contains':
+        if (fieldValue.includes(payload.rule.condition_value)) {
+          ruleValidationResponse(false, 'contains')
+        } else {
+          ruleValidationResponse(true, 'contains')
+        }
+    }
+  } catch {
+    genericErrorResponse('Invalid JSON payload passed.')
   }
 })
 export default router
