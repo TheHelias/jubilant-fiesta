3ca9f0dcfab20a4f89d92209161b71677a1168fb
TheHelias <akande.tolu@yahoo.com>
2021-01-29 10:08:33 +0100

    feat: catch non-existing routes

--- server/app.js
+++ server/app.js
@@ -1,15 +1,18 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
 
-import indexRouter from './routes/index';
+import indexRouter from './routes/index'
 
-var app = express();
+var app = express()
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use('/', indexRouter);
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use('/', indexRouter)
+app.use((req, res) => {
+  res.status(404).send({ message: 'not found' })
+})
 
-export default app;
+export default app
